e56583cfe4c5ade14ec32d38ce457080799dbc25
GitBoi37 <45974904+GitBoi37@users.noreply.github.com>
2019-05-01 08:10:31 -0700

    Create SnakeDriver.java

---
+++ snake/SnakeDriver.java
@@ -0,0 +1,25 @@
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class SnakeDriver extends JFrame{
+	private static final long serialVersionUID = 1L;
+	public SnakeDriver(){
+        initUI();
+    }
+    private void initUI(){
+        add(new Snake());
+        pack();
+        setResizable(false);
+        pack();
+        setTitle("Dear god... so many rectangles");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+    }
+    public static void main(String[] args){
+        EventQueue.invokeLater(() -> { 
+            JFrame ex = new SnakeDriver();
+            ex.setVisible(true);
+        });
+    }
+}
